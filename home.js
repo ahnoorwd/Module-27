643be2c6047b1cdd384f3497f43e284ba62a6924
ahmed nur <ahmednur103308@gmail.com>
2024-09-19 09:31:12 +0600

    Added the Add money option

--- home.js
+++ home.js
@@ -1,1 +1,24 @@
-console.log('home addeded')
+document.getElementById('add-money-button').addEventListener('click',function(event){
+    event.preventDefault();
+    const inputNumber=document.getElementById('input-ammuont').value;
+    // console.log(inputNumber);
+
+    const inputPin=document.getElementById('input-pin').value;
+    // console.log(inputPin);
+
+    if (inputPin==='1234'){
+        console.log('add money completely');
+        const balance=document.getElementById('raw-balance').innerText;
+        // console.log(balance);
+        const userMoney=parseFloat(inputNumber);
+        const oldMoney=parseFloat(balance);
+        const updatedMoney=userMoney+oldMoney;
+        // console.log(userMoney);
+        // const updatedValance=balance+inputNumber;
+         console.log(updatedMoney);
+         document.getElementById('raw-balance').innerText=updatedMoney;
+    }
+    else{
+        alert('failed to add money.try again');
+    }
+})
